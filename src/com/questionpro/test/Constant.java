b7a0a1a9781fef78aee16835d8c8acb8aaef9b07
dineshsatelkar <56499538+dineshsatelkar-11@users.noreply.github.com>
2021-01-18 21:39:51 +0530

    Create Constant.java

---
+++ src/com/questionpro/test/Constant.java
@@ -0,0 +1,8 @@
+package com.questionpro.test;
+
+public final class  Constant {
+	 public static final String NO_OF_FRIENDS = "NOOFFRIENDS";
+	 public static final String NO_OF_UNIQUE_JUICE = "NOOFUNIQUEJUICE";
+	 public static final String CHECK_NO_ONLY = "CHECKNOONLY";
+	public static final String CHECK_NO_ONLY_FOR_INPUT_TYPE = "CHECKNOONLYFORINPUTTYPE";
+}
